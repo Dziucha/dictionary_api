6f9f6a26bedd77badd4be2016e21ff653edeb327
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-22 23:38:40 +0100

    definition from dictionary

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def home():
     return render_template("home.html")
 
 
+df = pd.read_csv("dictionary.csv")
+
+
 @app.route("/api/v1/<word>")
 def dictionary(word):
-    df = pd.read_csv("dictionary.csv")
 
     definition = df.loc[df["word"] == word]["definition"].squeeze()
 
